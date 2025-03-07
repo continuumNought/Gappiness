1de970b42480230bea58d5cc1cf0ca3698645977
Chris La Valle <lavalle.l.c@gmail.com>
2025-03-07 00:08:07 -0600

    Streamlines code

--- src/hyperparameter_search_1.py
+++ src/hyperparameter_search_1.py
@@ -1,3 +1,4 @@
+import numpy as np
 import optuna
 import torch
 import time
@@ -5,16 +6,17 @@ import functools as ft
 from torch.utils.tensorboard import SummaryWriter
 from gappiness.loss import FastJacobianRegularizedLoss, MSELossWrapper
 from gappiness.model import Autoencoder
-from gappiness.data import load_data, standard_normalize
-from gappiness.noise import add_2d_gaussian_noise
+from gappiness.data import load_data, whiten
+from gappiness.noise import add_gaussian_noise
 from multiprocessing import Process
 from optuna.samplers import RandomSampler
 
 
-class ConstrainedSampler(RandomSampler):
-    def __init__(self, base_sampler=None):
+class NodeVolumeLimitSampler(RandomSampler):
+    def __init__(self, volume, base_sampler=None):
         super().__init__()
         self.base_sampler = base_sampler or RandomSampler()
+        self.volume = volume
 
     def sample_independent(self, study, trial, param_name, param_distribution):
         while True:
@@ -24,10 +26,12 @@ class ConstrainedSampler(RandomSampler):
             encoded_dim = trial.params.get("encoded_dim", value if param_name == "encoded_dim" else None)
             hidden_layers = trial.params.get("hidden_layers", value if param_name == "hidden_layers" else None)
 
-            if hidden_dim is not None and encoded_dim is not None and hidden_dim < encoded_dim:
+            if hidden_dim is not None and encoded_dim is not None \
+                and hidden_dim < encoded_dim:
                 continue
 
-            if hidden_dim is not None and hidden_layers is not None and hidden_dim*hidden_layers > 1000:
+            if hidden_dim is not None and hidden_layers is not None \
+                and hidden_dim*hidden_layers > self.volume:
                 continue
 
             return value
@@ -67,18 +71,60 @@ class EarlyStop:
         return self.misses >= self.patience
 
 
-def objective_factory(data_path, input_dim, max_epochs=100):
+def objective_factory(data_path, input_dim, max_epochs=100, **kwargs):
+    hyperparam_sample_args = {
+        'batch_sz': {'args': ('batch_sz', [16, 32, 64]), 'kwargs': {}},
+        'hidden_dim': {'args': ('hidden_dim', 5, 205), 'kwargs': {'step':10}},
+        'encoded_dim': {'args': ('encoded_dim', 5, 105), 'kwargs': {'step':10}},
+        'hidden_layers': {'args': ('hidden_layers', 2, 20), 'kwargs': {'step': 2}},
+        'lr': {'args': ('learning_rate', 1e-5, 1e-2), 'kwargs': {'log': True}},
+        'momentum': {'args': ('momentum', 0.5, 0.9), 'kwargs': {}},
+        'weight_decay': {'args': ('weight_decay', 1e-6, 1e-2), 'kwargs': {'log': True}},
+        'loss_fn': {'args': ('loss_fn', ['MSE', 'FJL']), 'kwargs': {}},
+        'sigma_sqr': {'args': ('sigma_sqr', 1e-6, 1e-2), 'kwargs': {'log': True}},
+    }
+
+    for k in kwargs:
+        hyperparam_sample_args[k] = kwargs[k]
+
     def objective(trial):
         hyperparams = {
-            'batch_sz' : trial.suggest_categorical('batch_sz', [16,32,64]),
-            'hidden_dim' : trial.suggest_int('hidden_dim', 5, 205, step=10),
-            'encoded_dim' : trial.suggest_int('encoded_dim', 5, 105, step=10),
-            'hidden_layers' : trial.suggest_int('hidden_layers', 2, 20, step=2),
-            'lr' : trial.suggest_float('learning_rate', 1e-5, 1e-2, log=True),
-            'momentum' : trial.suggest_float('momentum', 0.5, 0.9),
-            'weight_decay' : trial.suggest_float('weight_decay', 1e-6, 1e-2, log=True),
-            'loss_fn' : trial.suggest_categorical('loss_fn', ['MSE', 'FJL']),
-            'sigma_sqr' : trial.suggest_float('sigma_sqr', 1e-6, 1e-2, log=True),
+            'batch_sz' : trial.suggest_categorical(
+                *hyperparam_sample_args['batch_sz']['args'],
+                **hyperparam_sample_args['batch_sz']['kwargs'],
+            ),
+            'hidden_dim' : trial.suggest_int(
+                *hyperparam_sample_args['hidden_dim']['args'],
+                **hyperparam_sample_args['hidden_dim']['kwargs'],
+            ),
+            'encoded_dim' : trial.suggest_int(
+                *hyperparam_sample_args['encoded_dim']['args'],
+                **hyperparam_sample_args['encoded_dim']['kwargs'],
+            ),
+            'hidden_layers' : trial.suggest_int(
+                *hyperparam_sample_args['hidden_layers']['args'],
+                **hyperparam_sample_args['hidden_layers']['kwargs'],
+            ),
+            'lr' : trial.suggest_float(
+                *hyperparam_sample_args['lr']['args'],
+                **hyperparam_sample_args['lr']['kwargs'],
+            ),
+            'momentum' : trial.suggest_float(
+                *hyperparam_sample_args['momentum']['args'],
+                **hyperparam_sample_args['momentum']['kwargs'],
+            ),
+            'weight_decay' : trial.suggest_float(
+                *hyperparam_sample_args['weight_decay']['args'],
+                **hyperparam_sample_args['weight_decay']['kwargs'],
+            ),
+            'loss_fn' : trial.suggest_categorical(
+                *hyperparam_sample_args['loss_fn']['args'],
+                **hyperparam_sample_args['loss_fn']['kwargs'],
+            ),
+            'sigma_sqr' : trial.suggest_float(
+                *hyperparam_sample_args['sigma_sqr']['args'],
+                **hyperparam_sample_args['sigma_sqr']['kwargs'],
+            ),
         }
 
         batch_sz = hyperparams['batch_sz']
@@ -86,7 +132,7 @@ def objective_factory(data_path, input_dim, max_epochs=100):
             data_path,
             batch_sz,
             0.01,
-            normalize=standard_normalize,
+            normalize=whiten,
         )
 
         hidden_dim = hyperparams['hidden_dim']
@@ -107,7 +153,7 @@ def objective_factory(data_path, input_dim, max_epochs=100):
         )
         loss = hyperparams['loss_fn']
         sigma_sqr = hyperparams['sigma_sqr']
-        cov_matrix = ((sigma_sqr, 0.0), (0.0, sigma_sqr))
+        cov_matrix = sigma_sqr*np.identity(input_dim)
         criterion = select_loss(loss, model, sigma_sqr)
 
         with SummaryWriter(log_dir=f'runs/optuna_trial_{trial.number}') as writer:
@@ -117,7 +163,7 @@ def objective_factory(data_path, input_dim, max_epochs=100):
                 model.train()
                 for batch in train_loader:
                     inputs = batch[0]
-                    noisy_inputs = add_2d_gaussian_noise(inputs, cov_matrix=cov_matrix)
+                    noisy_inputs = add_gaussian_noise(inputs, np.zeros(input_dim), cov_matrix)
                     targets = inputs
 
                     # Forward pass
@@ -139,7 +185,7 @@ def objective_factory(data_path, input_dim, max_epochs=100):
                 model.eval()
                 with torch.no_grad():
                     inputs = test_dataset.tensors[0]
-                    noisy_inputs = add_2d_gaussian_noise(inputs, cov_matrix=cov_matrix)
+                    noisy_inputs = add_gaussian_noise(inputs, np.zeros(input_dim), cov_matrix)
                     outputs = model(noisy_inputs)
                     test_loss = criterion(noisy_inputs, outputs, inputs).item()/sigma_sqr
                     writer.add_scalar("Loss/Test", test_loss, epoch)
@@ -164,36 +210,40 @@ def objective_factory(data_path, input_dim, max_epochs=100):
     return objective
 
 
-def run_study(study_name, storage, input_dim, data_path, n_warmup_steps=10, n_trials=100):
+def run_study(
+    study_name,
+    storage,
+    input_dim,
+    data_path,
+    objective_kwargs,
+    n_warmup_steps=10,
+    n_trials=100,
+    volume=1000,
+):
     study = optuna.create_study(
         direction="minimize",
         pruner=optuna.pruners.MedianPruner(n_warmup_steps=n_warmup_steps),
         study_name=study_name,
         storage=storage,
         load_if_exists=True,
-        sampler=ConstrainedSampler(),
+        sampler=NodeVolumeLimitSampler(volume),
     )
 
     study.optimize(
-        objective_factory(data_path, input_dim),
+        objective_factory(data_path, input_dim, **objective_kwargs),
         n_trials=n_trials,
         n_jobs=1,
     )
 
 
-def main():
-    tasks = 8
-    study_name = "DAE-Hyperparameter-Tuning"
-    storage = "sqlite:///dae-hp-tuning.db"
-    data_path = '../data/silva_data_1.npy'
-    input_dim = 2
-
+def tune(tasks, study_name, storage, data_path, input_dim, hyperparam_sample_args):
     study_runner = ft.partial(
         run_study,
         study_name,
         storage,
         input_dim,
         data_path,
+        hyperparam_sample_args,
         n_trials=12,
     )
 
@@ -215,5 +265,37 @@ def main():
     print(f'Best Loss: {study.best_value}')
 
 
+def tune1():
+    tasks = 8
+    study_name = "DAE-Hyperparameter-Tuning"
+    storage = "sqlite:///dae-hp-tuning.db"
+    data_path = '../data/silva_data_1.npy'
+    input_dim = 2
+
+    hyperparam_sample_args = {}
+    tune(tasks, study_name, storage, data_path, input_dim, hyperparam_sample_args)
+
+
+def tune2a():
+    tasks = 8
+    study_name = "DAE-Hyperparameter-Tuning-2A"
+    storage = "sqlite:///dae-hp-tuning-2A.db"
+    data_path = '../data/gapiness_dataset_2A_Dr.npy'
+    input_dim = 5
+
+    hyperparam_sample_args = {
+        'batch_sz': {'args': ('batch_sz', [16, 32, 64]), 'kwargs': {}},
+        'hidden_dim': {'args': ('hidden_dim', 5, 505), 'kwargs': {'step':10}},
+        'encoded_dim': {'args': ('encoded_dim', 1, 4), 'kwargs': {}},
+        'hidden_layers': {'args': ('hidden_layers', 3, 27), 'kwargs': {'step': 3}},
+        'lr': {'args': ('learning_rate', 1e-5, 1e-2), 'kwargs': {'log': True}},
+        'momentum': {'args': ('momentum', 0.5, 0.9), 'kwargs': {}},
+        'weight_decay': {'args': ('weight_decay', 1e-6, 1e-2), 'kwargs': {'log': True}},
+        'loss_fn': {'args': ('loss_fn', ['MSE', 'FJL']), 'kwargs': {}},
+        'sigma_sqr': {'args': ('sigma_sqr', 1e-6, 1e-2), 'kwargs': {'log': True}},
+    }
+    tune(tasks, study_name, storage, data_path, input_dim, hyperparam_sample_args)
+
+
 if __name__ == '__main__':
-    main()
+    tune1()
